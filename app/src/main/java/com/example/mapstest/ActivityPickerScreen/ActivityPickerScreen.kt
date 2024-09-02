d80b5121415a857e7d391404ff3d145aa6105368
Berken Sayilir <sayilir.berken@gmail.com>
2024-09-03 00:20:25 +0200

    MapManager with initial map camara on hard coded 4133

--- app/src/main/java/com/example/mapstest/ActivityPickerScreen/ActivityPickerScreen.kt
+++ app/src/main/java/com/example/mapstest/ActivityPickerScreen/ActivityPickerScreen.kt
@@ -6,14 +6,16 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.mapstest.Managers.CategoryManager
+import com.example.mapstest.Managers.MapManager
 
 @Composable
 fun ActivityPickerScreen(){
     val categoryManager: CategoryManager = viewModel()
+    val mapManager: MapManager = viewModel()
 
     Box(modifier = Modifier.fillMaxSize())
     {
-        CategoryMap()
+        CategoryMap(mapManager = mapManager)
         CategoryPicker(categoryManager = categoryManager)
     }
 }
--- app/src/main/java/com/example/mapstest/ActivityPickerScreen/CategoryMap.kt
+++ app/src/main/java/com/example/mapstest/ActivityPickerScreen/CategoryMap.kt
@@ -1,9 +1,23 @@
 package com.example.mapstest.ActivityPickerScreen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.example.mapstest.Managers.MapManager
 import com.google.maps.android.compose.GoogleMap
 
 @Composable
-fun CategoryMap() {
-    GoogleMap()
+fun CategoryMap(mapManager: MapManager) {
+    val cameraPositionState by mapManager.cameraPositionState.collectAsState()
+//    val currentLocation by mapManager.currentLocation.collectAsState()
+
+    Box(modifier = Modifier.fillMaxSize())
+    {
+        GoogleMap(cameraPositionState = cameraPositionState) {
+
+        }
+    }
 }
